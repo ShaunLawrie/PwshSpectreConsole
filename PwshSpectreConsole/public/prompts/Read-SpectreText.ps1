66e5b8e95e70afcccd71d35537c257090689865b
Shaun Lawrie <beatbophiphop@gmail.com>
2023-12-08 00:28:28 +1300

    Make default conditional

--- PwshSpectreConsole/public/prompts/Read-SpectreText.ps1
+++ PwshSpectreConsole/public/prompts/Read-SpectreText.ps1
@@ -22,10 +22,12 @@ function Read-SpectreText {
     [Reflection.AssemblyMetadata("title", "Read-SpectreText")]
     param (
         [string] $Question = "What's your name?",
-        [string] $DefaultAnswer = "Prefer not to say"
+        [string] $DefaultAnswer
     )
     $prompt = [Spectre.Console.TextPrompt[string]]::new($Question)
     $prompt.DefaultValueStyle = [Spectre.Console.Style]::new($script:DefaultValueColor)
-    $prompt = [Spectre.Console.TextPromptExtensions]::DefaultValue($prompt, $DefaultAnswer)
+    if($DefaultAnswer) {
+        $prompt = [Spectre.Console.TextPromptExtensions]::DefaultValue($prompt, $DefaultAnswer)
+    }
     return Invoke-SpectrePromptAsync -Prompt $prompt
 }
