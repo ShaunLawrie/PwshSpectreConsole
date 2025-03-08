fb550a1dfabb525a920a688c1e6b1c984663aae8
Shaun Lawrie <shaun.r.lawrie@gmail.com>
2025-03-08 19:23:39 +1300

    Remove unused validators since they're argument transformers now, fixes: - Format-SpectreTextPath Fails with style hashtable #64

--- PwshSpectreConsole.Tests/formatting/Format-SpectreTextPath.tests.ps1
+++ PwshSpectreConsole.Tests/formatting/Format-SpectreTextPath.tests.ps1
@@ -21,5 +21,18 @@ Describe "Format-SpectreTextPath" {
             Assert-MockCalled -CommandName "Write-AnsiConsole" -Times 1 -Exactly
             { Assert-OutputMatchesSnapshot -SnapshotName "Format-SpectreTextPath" -Output $testConsole.Output } | Should -Not -Throw
         }
+
+        It "Should format with a custom format" {
+            $renderable = Format-SpectreTextPath -Path "C:\Windows\System32\cmd.exe" -PathStyle @{
+                RootColor      = [Spectre.Console.Color]::Cyan2
+                SeparatorColor = [Spectre.Console.Color]::Aqua
+                StemColor      = [Spectre.Console.Color]::Orange1
+                LeafColor      = [Spectre.Console.Color]::HotPink
+            }
+            $renderable | Should -BeOfType [Spectre.Console.TextPath]
+            $renderable | Out-SpectreHost
+            Assert-MockCalled -CommandName "Write-AnsiConsole" -Times 1 -Exactly
+            { Assert-OutputMatchesSnapshot -SnapshotName "Format-SpectreTextPathCustom" -Output $testConsole.Output } | Should -Not -Throw
+        }
     }
 }
